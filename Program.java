5d9e2fb3d8a1ffc3c9125cd75ef77a0893cb3bc6
Violeta Miño Ginesta <dte.violett@gmail.com>
2023-03-19 17:12:58 -0300

    Commit inicial

---
+++ Program.java
@@ -0,0 +1,8 @@
+import Ejercicio1;
+
+class Program {  
+    public static void main(String args[]){  
+        Ejercicio1.EjecutarA();
+
+    }
+}
---
+++ ejercicios/Ejercicio2.java
@@ -0,0 +1,6 @@
+class Ejercicio1 {  
+    public static void Ejecutar(){  
+
+
+    }
+}
